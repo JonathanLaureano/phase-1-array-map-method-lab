95a83a54cf81445822f87fcc3a279a8122b8b2ff
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-01-11 16:29:18 -0500

    commit tree 813fe6734be49daac1b29ae91d33ce3e03d139d5

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((section) => {
+    const words = section.split(" ");
+    const wordsCapped = words.map((word) => 
+    word.charAt(0).toUpperCase() + word.slice(1))
+    const result = wordsCapped.join(" ");
+    return result;
+  })
 }
